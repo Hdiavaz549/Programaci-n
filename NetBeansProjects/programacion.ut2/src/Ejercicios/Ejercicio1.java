59936d78217c09caf658c0669188b74b3225dd5b
Hdiavaz549 <hdiavaz549@g.educaand.es>
2024-10-28 10:42:58 +0100

    Add files via upload

---
+++ NetBeansProjects/programacion.ut2/src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,21 @@
+package Ejercicios;
+
+
+/**
+ *
+ * @author 1DAW2425-05
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo!");
+        int x = -3;
+        int valorAbsoluto = Math.abs(x);
+        System.out.println(valorAbsoluto);
+    }
+
+}
---
+++ NetBeansProjects/programacion.ut2/src/Ejercicios/Ejercicio3.java
@@ -0,0 +1,17 @@
+
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args){
+        int edad;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe año actual");
+        int x = sc.nextInt();
+        System.out.println("Escribe año en que naciste");
+        int y = sc.nextInt();
+        edad = x - y;
+        System.out.println("Tienes: " + edad);
+}
+}
